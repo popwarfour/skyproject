1fa0d884c510ab011e682be09e74e0d4797ea4ee
popwarfour <popwarfour@hotmail.com>
2013-03-30 01:54:34 -0400

    Started index UI

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="author" content="Anders D Melen" />
+	<meta name="description" content="Caked Snowboards - Get Gear, Join The Contest, GetWitIt" />
+	<META NAME="Title" CONTENT="Caked Snowboards - Home">
+	<meta name="keywords" content="snow, snowboarding, skate, skating, riding, skateboarding, caked, get wit it,  getwitit, contest, raffle, Dave, Tafur" />
+	<link rel="stylesheet" href="mystyle.css" type="text/css" media="screen" />	
+	<link rel="stylesheet" href="button.css" type="text/css" media="screen" />
+	<link rel='stylesheet' href='development-bundle/themes/base/jquery.ui.all.css'>
+</head>
+<body>
+	<div  class='mainContainer'>
+		<div class='banner'>
+			Banner
+		</div>
+		<div class='menuContainer'>
+			<div class='menuLink-Left'>Home</div>
+			<div class='menuLink-Middle'>About</div>
+			<div class='menuLink-Middle'>Purchase</div>
+			<div class='menuLink-Right'>Contact</div>
+			<div style="clear: both;"></div>
+		</div>
+		<div class='contentPane'>
+			Content Pane
+		</div>
+		<div class='smallBanner'>
+			Small Banner
+		</div>
+		<div class='footer'>
+			Footer
+		</div>
+	</div>
+</body>
