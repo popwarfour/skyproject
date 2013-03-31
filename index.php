80941dd3f4a8b9ad6926695e631dd6ce64595cad
popwarfour <popwarfour@hotmail.com>
2013-03-31 19:31:31 -0400

    added syrup sizes to index

--- index.php
+++ index.php
@@ -3,17 +3,29 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="author" content="Anders D Melen" />
-	<meta name="description" content="Caked Snowboards - Get Gear, Join The Contest, GetWitIt" />
-	<META NAME="Title" CONTENT="Caked Snowboards - Home">
-	<meta name="keywords" content="snow, snowboarding, skate, skating, riding, skateboarding, caked, get wit it,  getwitit, contest, raffle, Dave, Tafur" />
+	<meta name="description" content="Vermonts fines maples syrup" />
+	<META NAME="Title" CONTENT="Twin Maple Sugarworks Vermont - Home">
+	<meta name="keywords" content="" />
 	<link rel="stylesheet" href="mystyle.css" type="text/css" media="screen" />	
 	<link rel="stylesheet" href="button.css" type="text/css" media="screen" />
 	<link rel='stylesheet' href='development-bundle/themes/base/jquery.ui.all.css'>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
+	<script src="cycle.js" ></script>
+	<script type="text/javascript">
+		$('#fade').cycle();
+		$('#slide').cycle(
+		{ 
+		    fx:      'turnRight', 
+		    delay:   -4000 
+		});
+	</script>
 </head>
 <body>
 	<div  class='mainContainer'>
 		<div class='banner'>
-			<img src='maple.png' id='bannerImage' />
+			<img src='images/digitalLogo.png' id='bannerImage' />
+			<p id='bannerText'>Twin Maple Sugarworks</p>
+			<p id='bannerSubtext'>Vermonts Finest Maple Syrup</p>
 		</div>
 		<div class='menuContainer'>
 			<a href='index.php'><div class='menuLink-Left-Selected'>Home</div></a>
@@ -23,10 +35,53 @@
 			<div style="clear: both;"></div>
 		</div>
 		<div class='contentPane'>
-			Content Pane
+			<div  id='slide' style='float:left;'>
+				<img class='contentImages' src='images/sugaring.jpg'/>
+				<img class='contentImages' src='images/lincoln.jpg' />
+				<img class='contentImages' src='images/vermont.jpg' />
+				<img class='contentImages' src='images/forest.jpg' />
+				<img class='contentImages' src='images/sugaringInFullEffect.jpg'/>
+			</div>
+			<div style='float: left;' class='contentPaneRight'>
+				<h2 style='padding: 0px; margin: 0px;'>Maple Syrup - Order Online!</h2>
+				<table border='0px' style='padding: 0px; margin: 0px;'>
+					<tr>
+						<td class='contentPaneRight-SyrupSize'>
+							<img style='margin-top: 50px;' src='images/sizes/syrup.png' width='19px' alt='half pint'/>
+							</br>
+							&#189; Pint
+						</td>
+						<td class='contentPaneRight-SyrupSize'>
+							<img style='margin-top: 40px;' src='images/sizes/syrup.png' width='29px' alt='half pint'/>
+							</br>
+							Pint
+						</td>
+						<td class='contentPaneRight-SyrupSize'>
+							<img style='margin-top: 30px;' src='images/sizes/syrup.png' width='39px' alt='half pint'/>
+							</br>
+							16oz
+						</td>
+						<td class='contentPaneRight-SyrupSize'>
+							<img style='margin-top: 10px;' src='images/sizes/syrup.png' width='49px' alt='half pint'/>
+							</br>
+							&#189; Gallon
+						</td>
+						<td class='contentPaneRight-SyrupSize'>
+							<img src='images/sizes/syrup.png' alt='half pint'/>
+							</br>
+							Gallon
+						</td>
+					</tr>
+				</table>
+				<h2 style='padding: 0px;  margin: 0px;'>Education</h3>
+				<p style='padding: 0px;  margin: 0px;'>
+					<a href='http://vermontmaple.org/'>Vermont Maple Sugar Association</a></br>
+					<a href='http://www.huffingtonpost.com/2011/12/23/maple-syrup-grades_n_1167767.html#s570988&title=Grade_A_Dark'>Syrup Grading System</a>
+				</p>
+			</div>
 		</div>
 		<div class='smallBanner'>
-			Small Banner
+			<p class='smallBannerTitle'>Latest From the Sugarbush</p>
 		</div>
 		<div class='footer'>
 			Footer
